2a1beb576ad00db1596a8c97eda8de9e8bb371f7
Dmitrii Bukreev <1stringsamurai@gmail.com>
2022-04-26 14:32:40 +0200

    Add some code, logs

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,14 @@
+DROP TABLE IF EXISTS EMPLOYEE;
+
+CREATE TABLE EMPLOYEE (
+                          id INT AUTO_INCREMENT  PRIMARY KEY,
+                          name VARCHAR(250) NOT NULL,
+                          lastname VARCHAR(250) NOT NULL,
+                          department VARCHAR(250) DEFAULT NULL
+);
 INSERT INTO EMPLOYEE (id,name,lastname,department)
-VALUES ('1', 'Evgeniy', 'Petrosyan',1),
-       ('2', 'Dmitrii', 'Bukreev',3),
-       ('3', 'Arnold', 'Schwarznegger',2),
-       ('4', 'Hans', 'Zimmer',2),
-       ('5', 'Vanessa', 'May',1);
+VALUES ('1', 'Evgeniy', 'Petrosyan','personal'),
+       ('2', 'Dmitrii', 'Bukreev','verkauf'),
+       ('3', 'Arnold', 'Schwarznegger','verkauf'),
+       ('4', 'Hans', 'Zimmer','personal'),
+       ('5', 'Vanessa', 'May','entwicklung');
