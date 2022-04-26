d264970cbe569bcc72e8326be5d640d0b9a2e82c
Dmitrii Bukreev <1stringsamurai@gmail.com>
2022-04-26 12:36:15 +0200

    Add some code

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,4 +3,4 @@ VALUES ('1', 'Evgeniy', 'Petrosyan','1'),
        ('2', 'Dmitrii', 'Bukreev','3'),
        ('3', 'Arnold', 'Schwarznegger','1'),
        ('4', 'Hans', 'Zimmer','2'),
-       ('5', 'Vanessa', 'May','3'),
+       ('5', 'Vanessa', 'May','3');
